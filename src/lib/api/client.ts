602deff6acce74083333e4a6270d1d1cd4c4e913
minoong <alsdndjdrktl@gmail.com>
2021-06-30 22:10:43 +0900

    conflict test

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -7,6 +7,7 @@ const instance = axios.create({
 
 instance.interceptors.request.use(
   (config) => {
+    console.log('ztestz');
     config.headers = {
       Authorization: `Bearer ${localStorage.getItem('com.naver.nid.access_token')?.split('.')[13]}`,
     };

8ca4b68f49317a7121438b22e31f694ceffc7bd9
minoong <alsdndjdrktl@gmail.com>
2021-06-30 23:18:47 +0900

    eee

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -7,7 +7,7 @@ const instance = axios.create({
 
 instance.interceptors.request.use(
   (config) => {
-    console.log(21);
+    console.log(1);
     console.log(1);
     console.log(2);
     console.log(12);

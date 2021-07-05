06e7663d52360d35e89845590d13aa05fddfa7f0
lmw <alsdndjdrktl@gmail.com>
2021-07-05 22:17:00 +0900

    daily redux toolkit

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import rootReducer from './rootReducer';
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: [...getDefaultMiddleware(), logger],
   devTools: process.env.NODE_ENV !== 'production',
 });
+
+export default store;
